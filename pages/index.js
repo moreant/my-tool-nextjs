2ec16d41245ae1ef9af0fc56f134643a79bf2aa0
moreant <moreant@foxmail.com>
2021-07-23 01:29:27 +0800

    head

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-export default function Home() {
+export default function Home () {
   return (
-    <div class="h-screen">
-      <div class="p-24">
+
+    <>
+      <div className="bg-blue-700 divide-y divide-black divide-opacity-10">
+        <div className="max-w-7xl mx-auto py-6 px-4 flex leading-5 ">
+          <Link href="/"><div className="text-white text-2xl cursor-pointer">麦兔</div></Link>
+          <div></div>
+        </div>
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl tracking-wide text-white sm:text-4xl">
+            <span className="block mb-1">使用工具何必那么复杂，</span>
+            <span className="block text-blue-300">移到工具，选择模式，粘贴复制。</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-indigo-50"
+              >
+                Get started
+              </a>
+            </div>·
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 text-center">
           <li class="flex w-64 min-w-full">
             <div class="text-xl text-gray-900 font-bold relative rounded-xl ring-1 ring-black ring-opacity-5 shadow-sm w-full p-12" href="">
@@ -50,7 +81,7 @@ export default function Home() {
           </li>
         </ul>
       </div>
-    </div>
+    </>
 
   )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,12 +3,43 @@ import Link from 'next/link'
 export default function FirstPost () {
   return (
     <>
+      <div className="bg-blue-700 divide-y divide-black divide-opacity-10">
+        <div className="max-w-7xl mx-auto py-6 px-4 flex leading-5 ">
+          <Link href="/"><div className="text-white text-2xl cursor-pointer">麦兔</div></Link>
+          <div></div>
+        </div>
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl tracking-wide text-white sm:text-4xl">
+            <span className="block mb-1">UUID</span>
+            <span className="block text-blue-300">通用唯一识别码</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-indigo-50"
+              >
+                Get started
+              </a>
+            </div>·
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">
-        <a>Back to home</a></Link>
+          <a>Back to home</a></Link>
       </h2>
     </>
-    
+
   )
 }
