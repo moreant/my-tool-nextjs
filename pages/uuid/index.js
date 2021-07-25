e3143a8bf604edca5126e1e3b2dfb3f988992259
moreant <moreant@foxmail.com>
2021-07-26 02:07:38 +0800

    优化科普栏

--- pages/uuid/index.js
+++ pages/uuid/index.js
@@ -62,13 +62,13 @@ export default function UUID () {
                   </svg>
                 </span>
                 <p className="ml-3 font-medium text-white truncate">
-                  <span className="md:hidden">通用唯一识别码</span>
+                  <span className="md:hidden">UUID: 通用唯一识别码</span>
                   <span className="hidden md:inline">Universally unique identifier（通用唯一识别码）</span>
                 </p>
               </div>
               <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
                 <a
-                  href="#"
+                  href="https://baike.baidu.com/item/UUID/5921266"
                   className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
                 >
                   了解更多
