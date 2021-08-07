e4f2c8a617f2e5f62df9a358731bbe526afc3af5
moreant <moreant@foxmail.com>
2021-08-07 09:05:30 +0800

    引入谷歌统计

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import Router from 'next/router'
 import 'tailwindcss/tailwind.css'
+
+import * as gtag from '../lib/gtag'
+
+// Notice how we track pageview when route is changed
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
 
 export default function App ({ Component, pageProps }) {
   return <Component {...pageProps} />
