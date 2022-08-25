a733dd8ac473b89af12d74aaffde387fb4e67e3d
莫居尘 <45537528+moreant@users.noreply.github.com>
2022-08-25 12:56:26 +0800

    实际网站标题与备案网站名称不一致

--- components/page/Nav.js
+++ components/page/Nav.js
@@ -5,7 +5,7 @@ const Nav = props => {
     <>
       <div className="bg-blue-600 divide-y divide-black divide-opacity-10">
         <div className="max-w-7xl 2xl:max-w-screen-2xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex leading-5 ">
-        <Link href="/"><a className="text-white text-2xl cursor-pointer">麦兔</a></Link>
+        <Link href="/"><a className="text-white text-2xl cursor-pointer">麦兔 - 驿站开发</a></Link>
         </div>
         {props.children}
       </div>
